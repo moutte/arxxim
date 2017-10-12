3fac01e8ece47450d1e537abce8c75022bc12801
moutte <moutte.okada@gmail.com>
2017-10-12 15:45:04 +0200

    dtb tools

--- tmp/dtb_phreeqc.py
+++ tmp/dtb_phreeqc.py
@@ -31,6 +31,9 @@ def ChargeReplace(w):
   return w
 #--------------------------------------------------------//ChargeReplace
 
+Xmin,Xmax,Xdim= 0.,150.,7
+Tser= plt.linspace(Xmin,Xmax,num=Xdim)
+
 #---------------------------------------------------read PRIMARY species
 listPrim= []
 for line in f:
@@ -42,25 +45,29 @@ for line in f:
   if "=" in ww: listPrim.append(ww[2])
     
 for prim in listPrim: print prim
-raw_input()    
+raw_input("ENT")    
 #-------------------------------------------------//read PRIMARY species
 
 #---------------------------------------------read SECONDARY aqu'species
-listGamm= [] # open(s+".gam",'w')
+# listGamm= [] # open(s+".gam",'w')
 listAnly= [] # open(s+".anl",'w')
 listReac= []
-listLogK= []
+listLogK= []  ;  listNamK= []
 listSeco= []
+listName= []
+listForm= []
+listStok= []
 
-def stoikio(s):
+def species_scan(s):
   ww= s.split()
   coeffs=[]
   specis=[]
   for i,w in enumerate(ww):
-    if i%2==0: coeffs.append(w)
+    if i%2==0: coeffs.append(float(w))
     else:      specis.append(w)
   return coeffs,specis
 
+#------------read analytical_expression of sec'species and compute logks
 fi= open("logk_cemdata07_phreeqc_orig.new",'r')
 for line in fi:
   line= line.strip()
@@ -68,25 +75,16 @@ for line in fi:
   ww= line.split()
   if ww[0]=="PHASES": break
   if '=' in line: nam= line.split('=')[1]
-  if ww[0]== "-analytical_expression": listAnly.append((nam,ww[1:]))
-
-Xmin,Xmax,Xdim= 0.,150.,7
-Tser= plt.linspace(Xmin,Xmax,num=Xdim)
-fLogk= open(fData+".logk",'w')
-for rec in listAnly:
-  coefs=[]
-  nam, vec= rec
-  for w in vec:
-    coefs.append(float(w))
-  print nam,len(coefs),coefs
-  logKs=[]
-  for T in Tser:
-    TK= T + 273.15
-    logKs.append(-logK(coefs,TK)) # MINUS SIGN !!!
-  fLogk.write("%s\t" % nam)
-  for x in logKs: fLogk.write("%.4g\t" % x)
-  fLogk.write('\n')
-raw_input()
+  if ww[0]== "-analytical_expression":
+    listAnly.append((nam,ww[1:]))
+    coefs=[float(w) for w in ww[1:]]
+    logKs=[]
+    for T in Tser:
+      TK= T + 273.15
+      logKs.append(-logK(coefs,TK)) # MINUS SIGN !!!
+    listLogK.append(logKs)
+    listNamK.append(nam)
+#---------------------------------------------------------------------//
 
 for line in f:
   line= line.strip()
@@ -98,46 +96,48 @@ for line in f:
   if '#' in ww:
     i= ww.index('#')
     ww= ww[0:i+1]
-  if   ww[0]== "-gamma":                 listGamm.append(ww[1:])
+  if   ww[0]== "-gamma":                 continue #listGamm.append(ww[1:])
   elif ww[0]== "-analytical_expression": continue #listAnly.append(ww[1:])
-  elif ww[0].lower()== "log_k":          listLogK.append(ww[1:])
+  elif ww[0].lower()== "log_k":          continue #listLogK.append(ww[1:])
   elif "=" in ww:
     ww= line.split('=')
+    ww= [w.strip() for w in ww]
     listReac.append(ww[0])
     listSeco.append(ww[1])
-    coeffs,specis= stoikio(ww[0])
-    print ww[1], specis
-raw_input()
+    listName.append(ww[1])
+    listForm.append(ww[1])
+#raw_input()
 
-for reac in listReac:
-  print reac
-  #raw_input()
-  
-fo= open(fData+"_aqu.tab",'w')
-for prim in listPrim:
-  fo.write("%s\n" % prim)
-for i,reac in enumerate(listReac):
-  fo.write("%s\t%s\t" % (listSeco[i],reac))
-  # for w in listAnly[i]: fo.write("%s\t" % w)
-  fo.write("\n")
-fo.close()
+if 0:
+  fo= open(fData+"_aqu.tab",'w')
+  for prim in listPrim:
+    fo.write("%s\n" % prim)
+  for i,reac in enumerate(listReac):
+    fo.write("%s\t%s\t" % (listSeco[i],reac))
+    # for w in listAnly[i]: fo.write("%s\t" % w)
+    fo.write("\n")
+  fo.close()
 #-------------------------------------------//read SECONDARY aqu'species
 
 #------------------------------------------------------------read PHASES
-listGamm= [] # open(s+".gam",'w')
-listAnly= [] # open(s+".anl",'w')
-listReac= []
-listLogK= []
-listName= []
-listForm= []
-
+#------------read analytical_expression of sec'species and compute logks
 for line in fi:
   line= line.strip()
   if not line.strip(): continue
   ww= line.split()
   #if ww[0]=="PHASES": break
   if '=' in line: nam= line.split('=')[0]
-  if ww[0]== "-analytical_expression": listAnly.append((nam,ww[1:]))
+  if ww[0]== "-analytical_expression":
+    listAnly.append((nam,ww[1:]))
+    coefs=[]
+    for w in ww[1:]: coefs.append(float(w))
+    logKs=[]
+    for T in Tser:
+      TK= T + 273.15
+      logKs.append(-logK(coefs,TK)) # MINUS SIGN !!!
+    listLogK.append(logKs)
+    listNamK.append(nam)
+#---------------------------------------------------------------------//
 
 for line in f:
   line= line.strip()
@@ -148,11 +148,9 @@ for line in f:
   if '#' in ww:
     i= ww.index('#')
     ww= ww[0:i+1]
-  if   ww[0]== "-gamma":
-    listGamm.append(ww[1:-1])
-  elif ww[0]== "-analytical_expression": continue #listAnly.append(ww[1:-1])
-  elif ww[0].lower()== "log_k":
-    listLogK.append(ww[1:-1])
+  if   ww[0]== "-gamma": continue # listGamm.append(ww[1:-1])
+  elif ww[0]== "-analytical_expression": continue # listAnly.append(ww[1:-1])
+  elif ww[0].lower()== "log_k": continue # listLogK.append(ww[1:-1])
   elif "=" in ww:
     ww= line.split('=')
     listReac.append(ww[1])
@@ -161,9 +159,58 @@ for line in f:
     listName.append(line.strip())
   #if ww[0]=="PHASES": break
 
-for reac in listReac:
-  print reac
-raw_input()
+#---------------stoikio of sec'species with respect to prim'species only
+print "==sec'species stoikio=="
+for i,reac in enumerate(listReac):
+  coeffs,specis= species_scan(reac)
+  print listName[i],coeffs, specis
+  stoikio= [0. for sp in listPrim]
+  for j,sp in enumerate(specis):
+    if sp in listPrim:
+      stoikio[listPrim.index(sp)]= coeffs[j]
+  for j,sp in enumerate(specis):
+    if sp in listSeco:
+      isec= listSeco.index(sp)
+      if isec<i:
+        for k in range(len(stoikio)):
+          kk= listStok[isec][k]
+          if kk!=0:
+            stoikio[k]= stoikio[k] + listStok[isec][k]*coeffs[j]
+            for T in range(len(Tser)):
+              listLogK[i][T]= listLogK[i][T] + kk*listLogK[isec][T]
+      else:
+        print sp, "=species without stoik"
+    elif not sp in listPrim:
+      print sp, "=species not found"
+  print stoikio
+  listStok.append(stoikio)
+  # raw_input()
+raw_input("ENT")
+#---------------------------------------------------------------------//
+
+fLogk= open(fData+".logk",'w')
+for i,logk in enumerate(listLogK):
+  fLogk.write("%s\t" % listNamK[i])
+  for x in logk: fLogk.write("%.4g\t" % x)
+  fLogk.write('\n')
+raw_input("ENT")
+
+#-----------------------------------------------------write stokio table
+fo= open(fData+"_stoikio.tab",'w')
+fo.write(".formula\t")
+for prim in listPrim: fo.write("%s\t" % prim)
+fo.write("\n")
+for i,frm in enumerate(listForm):
+  fo.write("%s\t" % frm)
+  for x in listStok[i]: fo.write("%.4g\t" % x)
+  fo.write("\n")
+fo.close()
+#---------------------------------------------------------------------//
+  
+if 0:
+  for reac in listReac:
+    print reac
+  raw_input()
   
 fo= open(fData+"_min.tab",'w')
 for i,reac in enumerate(listReac):
@@ -175,12 +222,13 @@ fo.close()
 
 sys.exit()
 
-if 0:
+if 1:
   fi= open("logk_cemdata07_phreeqc.orig",'r')
   fo= open("logk_cemdata07_phreeqc_orig.new",'w')
   for line in fi:
-    if not line.strip(): continue
-    if line.strip()[0]=='#': continue
+    line= line.strip()
+    if not line: continue
+    if line[0]=='#': continue
     if ';' in line:
       ww= line.split(';')
       for w in ww: fo.write(w.strip()+'\n')
